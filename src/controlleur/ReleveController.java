08329142485d1c1670897c6fda1ff063dc61f11b
Clément <clement.maleterre@etu.unilim.fr>
2016-01-12 17:26:08 +0100

    Ajout des controle sur seuil haut/bas en fonction des marqueurs (non testé)

--- src/controlleur/ReleveController.java
+++ src/controlleur/ReleveController.java
@@ -27,20 +27,44 @@ public class ReleveController {
 	public static double controller(int num, double releve, String commentaire) {
 		
 		JsonObject station = tabStations[num];
-		double seuilHaut = station.getInt("seuilHaut");
-		double seuilBas = station.getInt("seuilBas");
+		double seuilHaut;
+		double seuilBas;
 		double result=0.0;
-		
-		
-		if(releve < seuilBas){
-			result= seuilBas-releve;
-		}
-		else if(releve > seuilHaut){
-			result= releve-seuilHaut;
-		}
-		enregistrer(station.getInt("idStation"),releve,commentaire);
 		DecimalFormat format=new DecimalFormat();
 		format.setMaximumFractionDigits(2);
+		
+		if(station.getString("marqueur").equals("110") || station.getString("marqueur").equals("111")){
+			seuilHaut = station.getInt("seuilHaut");
+			seuilBas = station.getInt("seuilBas");
+			if(releve < seuilBas){
+				result= seuilBas-releve;
+			}
+			else if(releve > seuilHaut){
+				result= releve-seuilHaut;
+			}
+			else{
+				enregistrer(station.getInt("idStation"),releve,commentaire);
+			}
+		}
+		else if(station.getString("marqueur").substring(0, 0).equals("0") && station.getString("marqueur").substring(1, 1).equals("1")){
+			seuilBas = station.getInt("seuilBas");
+			if(releve < seuilBas){
+				result= seuilBas-releve;
+			}
+			else{
+				enregistrer(station.getInt("idStation"),releve,commentaire);
+			}
+		}
+		else if(station.getString("marqueur").substring(0, 0).equals("1") && station.getString("marqueur").substring(1, 1).equals("0")){
+			seuilHaut = station.getInt("seuilHaut");
+			if(releve > seuilHaut){
+				result= releve-seuilHaut;
+			}
+			else{
+				enregistrer(station.getInt("idStation"),releve,commentaire);
+			}
+		}
+		
 		return Double.parseDouble(format.format(result).replace(",", "."));
 	}
 	/**
@@ -51,6 +75,23 @@ public class ReleveController {
 	 */
 	public static void enregistrer(int num, double releve, String commentaire) {
 		JsonController.ecrireReleve(nomFichier, num, commentaire, releve);
+	}
+	
+	public static String seuil(int num){
+		JsonObject station = tabStations[num];
+		String seuil="";
+		if (station.getString("marqueur").substring(1, 1).equals("1")){
+			seuil+="Seuil Bas: "+station.getInt("seuilBas")+ "		";
+		}
+		
+		if(station.getString("marqueur").substring(0, 0).equals("1")){
+			seuil+="Seuil Haut: "+station.getInt("seuilHaut")+ "		";
+		}
+		
+		if(station.getString("marqueur").substring(2, 2).equals("1")){
+			seuil+="Valeur Normale: "+station.getInt("valeurNormale");;
+		}
+		return seuil;
 	}
 
 }
