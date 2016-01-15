961bc4fe8463500814978f1f8c8a9a6c76231214
TomDarneix <tomdar87@gmail.com>
2016-01-15 10:06:25 +0100

    Graphique (plus ou moins) fonctionnel

--- src/controleur/GraphiqueStationController.java
+++ src/controleur/GraphiqueStationController.java
@@ -1,28 +1,41 @@
 package controleur;
 
+import java.util.Comparator;
+
+import javax.swing.event.ListSelectionEvent;
+
 import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.AnchorPane;
 import modele.Releve;
 import modele.Station;
 
 public class GraphiqueStationController {
 	@FXML
+	AnchorPane racine;
+	
+	@FXML
 	ComboBox<Station> listeStation;
 	
 	@FXML
-	LineChart<Number, Number> graph;
+	LineChart<String,Number> graph;
 	
 	@FXML
 	Button exit;
 	
 	ObservableList<Station> stations=StationController.loadStation(1);
-	XYChart.Series<Number, Number> series;
+	XYChart.Series<String, Number> series;
 	
 	@FXML
 	/**
@@ -30,25 +43,55 @@ public class GraphiqueStationController {
 	 */
 	private void initialize() {
 		listeStation.setItems(stations);
-		graph.setVisible(false);
 	}
 	public void Graphique(){
-		graph.setVisible(true);
+		if(racine.getChildren().contains(graph)){
+			racine.getChildren().remove(graph);
+		}
 		int idStationSelected=listeStation.getSelectionModel().getSelectedItem().getId();//pas ça
-		ObservableList<Releve> releve=ReleveController.loadRelevesForStation(idStationSelected);//pas ça
-		NumberAxis yAxis = new NumberAxis();
-		NumberAxis xAxis = new NumberAxis();
-		xAxis.setLabel("Date");//pas ça
-		yAxis.setLabel("Valeurs");//pas ça
-		series = new XYChart.Series<>();
-		graph = new LineChart<Number, Number>(xAxis, yAxis);
-		series.getData().add(new Data<Number, Number>(1, 1));//pas ça
-		series.getData().add(new Data<Number, Number>(2, 2));//pas ça
-		series.getData().add(new Data<Number, Number>(3, 3));//pas ça
-		series.getData().add(new Data<Number, Number>(4, 4));//pas ça
-		series.getData().add(new Data<Number, Number>(5, 5));//pas ça
-		series.getData().add(new Data<Number, Number>(6, 6));//pas ça non plus
-		graph.getData().add(series);
+		ObservableList<Releve> releves=ReleveController.loadRelevesForStation(idStationSelected);//pas ça
+		final CategoryAxis xAxis = new CategoryAxis();
+	    final NumberAxis yAxis = new NumberAxis();
+	    xAxis.setLabel("Date");
+	    yAxis.setLabel("Valeurs");
+	    graph = new LineChart<String,Number>(xAxis, yAxis);
+
+	    graph.setTitle("Graphique "+listeStation.getSelectionModel().getSelectedItem().getNom());
+	    Series<String, Number> series = new XYChart.Series<String, Number>();
+	    series.setName("Releve");
+	    for(int i=releves.size()-1;i>=0;i--){
+	    	series.getData().add(new XYChart.Data<String, Number>(releves.get(i).getDate(), releves.get(i).getValeur()));
+	    }
+	    graph.getData().add(series);
+	    graph.setLayoutY(134.0);
+	    graph.setPrefHeight(316.0);
+	    graph.setPrefWidth(583.0);
+	    racine.getChildren().add(graph);
+	    
+	    for (XYChart.Series<String, Number> s : graph.getData()) {
+            for (Data<String, Number> d : s.getData()) {
+                Tooltip.install(d.getNode(), new Tooltip(
+                        d.getXValue().toString() + "\n" + 
+                "Valeur : " + d.getYValue()));
+
+                d.getNode().setOnMouseEntered(new EventHandler<Event>() {
+
+                    @Override
+                    public void handle(Event event) {
+                        d.getNode().getStyleClass().add("onHover");                     
+                    }
+                });
+
+                d.getNode().setOnMouseExited(new EventHandler<Event>() {
+
+                    @Override
+                    public void handle(Event event) {
+                        d.getNode().getStyleClass().remove("onHover");      
+                    }
+                });
+            }
+        }
+
 	}
 	
 	public void retour(){
